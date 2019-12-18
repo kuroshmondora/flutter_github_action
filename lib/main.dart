d9dc7a61891bf5e51bce65e2f72de6cd07d2930f
Kurosh <bazrafkan.mb@gmail.com>
2019-12-18 15:04:46 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "Hello world!",
+            style: GoogleFonts.pacifico(
+              textStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 22,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
